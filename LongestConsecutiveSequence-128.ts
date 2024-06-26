526edf1d96e82ffedf56c186f39798946260ff71
Nick <nikdanelia@gmail.com>
2024-06-26 16:15:24 +0400

    Longest consecutive sequence

---
+++ LongestConsecutiveSequence-128.ts
@@ -0,0 +1,14 @@
+function longestConsecutive(nums: number[]): number {
+  const numSet = new Set(nums);
+  let longest = 0;
+  for (let n of nums) {
+    if (!numSet.has(n - 1)) {
+      let length = 1;
+      while (numSet.has(n + length)) {
+        length++;
+      }
+      longest = Math.max(longest, length);
+    }
+  }
+  return longest;
+}
